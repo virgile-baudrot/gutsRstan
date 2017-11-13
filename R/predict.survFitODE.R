8154e9ad42dc70439719af269f082de355c03957
vbaudrot <virgile.baudrot@posteo.net>
2017-11-13 09:47:35 +0100

    Add function for prediction via package deSolve

--- R/predict.survFitODE.R
+++ R/predict.survFitODE.R
@@ -1,42 +1,68 @@
-#' PREDICTION
+#' Prediction
 #' 
 #' @param x An object of class \code{survFitODE}
 #' @param new_data A new data set with concentration time point (a data.frame with 2 column,
 #'  time in the first column and concentration in the second)
-#'  
+#' 
+#' @import deSolve
+#'    
+#' @export
+#' 
+#' 
+predict.survFitODE <- function(x, new_data = NULL,
+                               interpolate_length = 1e2,
+                               interpolate_method = "linear"){
 
-predict.survFitODE <- function(x, new_data){
-  
+    if(is.null(new_data)){
+      # run_odes on profiles used for estimations
 
-    tconcPredict <- new_data[,1]
-    concPredict <- new_data[,2]
-    # initial time must be less than t0 = 0, so we use a very small close small number -1e-8
-    y_predict[1:100,1:2] = solve_TKTD_varSD(y0, -1e-9, tPredict[1:100], param, tconcPredict[1:100], concPredict[1:100])
-    
-    Psurv_hat[1:100] = exp( - y_predict[1:100, 2])
-    
-    for(i in 1:100){
-      Conditional_Psurv_hat[i] =  ifelse(i == 1 , Psurv_hat[1] , Psurv_hat[i] / Psurv_hat[i-1])
-      
-      Nsurv_predict[i] = binomial_rng(Nprec[i], Conditional_Psurv_hat[i])
+      dataStan <-  x$dataStan
+
+      new_data <- data.frame(conc = dataStan$conc,
+                             time = dataStan$tconc,
+                             replicate = dataStan$replicate_conc)
+
+      new_data_list = split(new_data, new_data$replicate)
+
+    } else{
+      new_data_list = list(new_data = new_data)
     }
+
+    # list_ODES <- lapply(1:n_replicate, function(it)
+    #                     run_odes(x, new_data_list[[it]]))
     
-    
+    n_replicate <- length(unique(new_data$replicate))
   
+    list_ODEs <- lapply(1:n_replicate,
+                        function(it) {
+                          solve_TKTD(x,
+                                     new_data_list[[it]],
+                                     interpolate_length = interpolate_length,
+                                     interpolate_method = interpolate_method)
+                        })
+    
+    names(list_ODEs) <- unique(new_data$replicate)
+    return(list_ODEs)
+
+    # if(x$model_type == "SD"){
+    #   Nsurv_hat = exp( - odes_out)
+    # }
+    # if(x$model_type == "IT"){
+    #   Nsurv_hat = binomial()
+    # }
+    #
+    # return(list(psurv_hat = psurv_hat,
+    #             Nsurv_hat = Nsurv_hat))
+
 }
 
-# ----------
+# -------
 
-load(file = "tests/testthat/fit_varIT.rda")
-conc <- c(1,5,2,6,3,7,4,8,5,9,6)
-tconc <- 0:10 # DOIT COMMENCER A 0
-new_data_test = data.frame(tconc, conc)
-
-odes_out <- run_odes(x = fit_varIT, new_data  = new_data_test)
-
-# solve_ODE_for all parmaters
-expose_stan_functions("exec/ode_TKTD_varIT.stan")
-
+#' Run odes with the stan integrator
+#'
+#' @export
+#' 
+#' 
 run_odes <- function(x, new_data){
 
   # This is usefull to load the function ode_TKTD_varSD which has been implemented  
@@ -48,19 +74,162 @@ run_odes <- function(x, new_data){
   }
   df_stanEstim <- as.data.frame(stanEstim)
 
-  time_Predict <- seq(0, max(new_data[,1]), length.out = 100)
+  time_Predict <- seq(0, max(new_data[,1]), length.out = 1e2)
   
   if(x$model_type == "SD"){
     y0 = c(0, 0) # initial dose
-    ls_ode <- lapply(1:nrow(df_stanEstim), function(it) 
+    ls_ode <- lapply(1:nrow(df_stanEstim), function(it)
       solve_TKTD_varSD(y0, -1e-9, time_Predict, as.numeric(df_stanEstim[it,]), new_data[,1], new_data[,2]))
   }
   if(x$model_type == "IT"){
     y0 = 0
-    ls_ode <- lapply(1:nrow(df_stanEstim), function(it) 
+    ls_ode <- lapply(1:nrow(df_stanEstim), function(it)
       solve_TKTD_varIT(y0, -1e-9, time_Predict, as.numeric(df_stanEstim[it,]), new_data[,1], new_data[,2]))
   }
   
   return(ls_ode)
 }
+
+#' solver
+#' 
+#' 
+
+
+#' Forecasting ith ODE solver
+
+
+# ----------------------------
+
+model_SD <- function(t, State, parms, input)  {
+  with(as.list(c(parms, State)), {
+    
+    C = rep(input(t), length = n_size)
+    
+    D = State[1:n_size]
+    H = State[(n_size+1):(2*n_size)] 
+    
+    dD <- kd*(C - D)     # internal concentration
+    dH <- kk*pmax(0, D-z) + hb # risk function
+    
+    res <- c(dD, dH)
+    list(res, signal = input(t))
+    
+  })
+}
+
+model_IT <- function(t, State, parms, input) {
+  with(as.list(c(parms, State)), {
+    
+    C = rep(input(t), length = n_size)
+    
+    D = State[1:n_size]
+    
+    dD <- kd*(C - D)    # internal concentration
+    
+    list(dD = dD, signal = input(t))
+    
+  })
+}
+
+solve_TKTD <- function(x, 
+                       new_data,
+                       interpolate_length = 1e3,
+                       interpolate_method = "linear"){
+  
+  model_type = x$model_type
+
+  MCMC_stanEstim <- extract_MCMCparameters(x)
+  n_size <- nrow(MCMC_stanEstim)
+  ## external signal with several rectangle impulses
+  signal <- data.frame(times = new_data$time, 
+                       import = new_data$conc)
+  
+  sigimp <- approxfun(signal$times, signal$import, method = interpolate_method, rule = 2)
+  
+  if(!is.null(interpolate_length)){
+    times = seq(min(new_data$time),max(new_data$time), length = interpolate_length)
+  } else{
+    times = signal$times
+  }
+  
+  ## model
+  kd = 10^MCMC_stanEstim$kd_log10
+  hb = 10^MCMC_stanEstim$hb_log10
+    
+  if(model_type == "IT"){
+
+      alpha = 10^MCMC_stanEstim$alpha_log10
+      beta = 10^MCMC_stanEstim$beta_log10
+    
+    ## The parameters
+    parms  <- list( kd = kd,
+                    alpha = alpha,
+                    beta = beta,
+                    n_size = n_size)
+    
+    ## Start values for steady state
+    xstart <- c(D = rep(0,n_size))
+    
+    ## Solve model
+    out <- ode(y = xstart,
+               times = times,
+               func = model_IT,
+               parms,
+               input = sigimp)
+    
+  }
+  if (model_type == "SD"){
+
+      kk = 10^MCMC_stanEstim$kk_log10
+      z = 10^MCMC_stanEstim$z_log10
+    
+    ## The parameters
+    parms  <- list( kd = kd,
+                    hb = hb,
+                    kk = kk,
+                    z = z,
+                    n_size = n_size)
+    
+    ## Start values for steady state
+    xstart <- c(D = rep(0,n_size),
+                H = rep(0,n_size))
+    
+    ## Solve model
+    out <- ode(y = xstart,
+               times = times,
+               func = model_SD,
+               parms,
+               input = sigimp)
+  }
+  
+  if(model_type == "IT"){
+    D_matrix = as.data.frame(out) %>%
+      dplyr::select(-c(time,signal))%>%
+      as.matrix()
+    S <- 1-plogis(log(t(D_matrix)), location = log(parms$alpha), scale = 1/parms$beta)
+    dtheo <- t(S)
+    
+  }
+  if(model_type=="SD"){
+    
+    H_matrix = as.data.frame(out) %>%
+      dplyr::select(contains("H"),-c(time,signal))%>%
+      as.matrix()
+    S <- exp(-H_matrix)
+    dtheo <- S
+  }
+
+  # -------- df.theo
+  df <- data_frame(time = out[, "time"],
+                  conc = out[, "signal"],
+                  replicate = rep(unique(new_data$replicate), length(out[, "time"])),
+                  q50 = apply(dtheo, 1, quantile, probs = 0.5, na.rm = TRUE),
+                  qinf95 = apply(dtheo, 1, quantile, probs = 0.025, na.rm = TRUE),
+                  qsup95 = apply(dtheo, 1, quantile, probs = 0.975, na.rm = TRUE))
+  ## RETURN
+  return(df)
+}
+
+
+
 
