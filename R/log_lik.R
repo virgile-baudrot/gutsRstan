1d8ec9d8a60df8d0e659291d7000b89f65868a2f
vbaudrot <virgile.baudrot@posteo.net>
2017-11-24 17:28:36 +0100

    add log likelihood

---
+++ R/log_lik.R
@@ -0,0 +1,96 @@
+#' Generic function for pointwise log-likelihood
+#'
+#' We define a new function \code{log_lik} rather than a
+#' \code{\link[stats]{logLik}} method because (in addition to the conceptual
+#' difference) the documentation for \code{logLik} states that the return value
+#' will be a single number, whereas \code{log_lik} returns a matrix. See this 
+#' package \pkg{rstanTKTD} the function \code{\link[rstanTKTD]{log_lik.stanTKTD}}
+#' or \code{\link[rstanarm]{log_lik.stanreg}} in the \pkg{rstanarm} package
+#' for an example.
+#'
+#' @export
+#'
+#' @return \code{log_lik} methods should return a \eqn{S} by \eqn{N} matrix,
+#'   where \eqn{S} is the size of the posterior sample (the number of draws from
+#'   the posterior distribution) and \eqn{N} is the number of data points.
+#'
+#'
+#' @examples
+#' # See help("log_lik", package = "rstanTKTD")
+#'
+log_lik <- function(object, ...) {
+  UseMethod("log_lik")
+}
+
+#' Pointwise log-likelihood matrix
+#'
+#' For models fit using MCMC only, the \code{log_lik} method returns the
+#' \eqn{S} by \eqn{N} pointwise log-likelihood matrix, where \eqn{S} is the size
+#' of the posterior sample and \eqn{N} is the number of data points.
+#'
+#' @aliases log_lik
+#' @export
+#'
+#' @param newdata An optional data frame of new data (e.g. holdout data) to use
+#'   when evaluating the log-likelihood. See the description of \code{newdata}
+#'   for \code{\link{posterior_predict}}.
+#'   
+#' @return For the \code{stanTKTD} method an \eqn{S} by 
+#'   \eqn{N} matrix, where \eqn{S} is the size of the posterior sample and 
+#'   \eqn{N} is the number of data points.
+#'   
+#'   
+#' @examples 
+#' \donttest{
+#' 
+#' }
+#'
+#'
+log_lik.stanTKTD <- function(object,
+                             newdata = NULL,
+                             ...){
+  
+  # Observation
+  df_obs <- data.frame(Nsurv = object$dataStan$Nsurv,
+                       tNsurv = object$dataStan$tNsurv,
+                       replicate = object$dataStan$replicate_Nsurv) %>%
+    group_by(replicate) %>%
+    dplyr::mutate(Nsurv_post = ifelse( tNsurv == max(tNsurv), 0, dplyr::lead(Nsurv)),
+                  Nsurv_diff = Nsurv - Nsurv_post,
+                  nrow_label = row_number(),
+                  max_tNsurv = max(tNsurv))
+  
+  # Estimation of probabiblity of survival
+  
+  mat_psurv <- t(rstan::extract(object$stanfit, pars = c("Psurv_hat"))$Psurv_hat)
+  
+  mat_psurv_post <- matrix(nrow = nrow(mat_psurv), ncol = ncol(mat_psurv))
+  
+  for(i in 1:nrow(mat_psurv)){
+    if(df_obs$tNsurv[i] == df_obs$max_tNsurv[i]){
+      mat_psurv_post[i, 1:ncol(mat_psurv)] = rep(0, ncol(mat_psurv))
+    } else{
+      mat_psurv_post[i, 1:ncol(mat_psurv)] = mat_psurv[(i+1), 1:ncol(mat_psurv)]
+    }
+  }
+  mat_psurv_diff <- mat_psurv - mat_psurv_post
+  
+  # Computing of the likelihood
+  mat_log_lik <- df_obs$Nsurv_diff * log(mat_psurv_diff)
+  
+  ## For the package loo, the matrix as to be An S by N matrix, where
+  ## S is the size of the posterior sample (the number of simulations) and
+  ## N is the number of data points.
+  transpose_mat_log_lik <-  t(mat_log_lik)
+
+  return(transpose_mat_log_lik)
+}
+
+
+# Vectorized log_likelihood
+
+matTovec_loglik <- function(mat_log_lik){
+  
+  vec_log_lik <- apply(mat_log_lik, 2, sum)
+  
+}
