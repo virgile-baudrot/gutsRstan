c080318bfc8ee4bd7aa698a058657796936d80c0
vbaudrot <virgile.baudrot@posteo.net>
2017-11-28 16:41:25 +0100

    log likelihood

--- R/log_lik.R
+++ R/log_lik.R
@@ -18,7 +18,7 @@
 #' @examples
 #' # See help("log_lik", package = "rstanTKTD")
 #'
-log_lik <- function(object, ...) {
+log_lik <- function(x, ...) {
   UseMethod("log_lik")
 }
 
@@ -46,37 +46,67 @@ log_lik <- function(object, ...) {
 #' }
 #'
 #'
-log_lik.stanTKTD <- function(object,
+log_lik.stanTKTD <- function(x,
                              newdata = NULL,
+                             draws = NULL,
+                             interpolate_length = 1e2,
+                             interpolate_method = "linear",
+                             mc.cores = 1,
                              ...){
   
   # Observation
-  df_obs <- data.frame(Nsurv = object$dataStan$Nsurv,
-                       tNsurv = object$dataStan$tNsurv,
-                       replicate = object$dataStan$replicate_Nsurv) %>%
+  if(is.null(newdata)){
+    x_data =  x$data
+  } else{
+    x_data = newdata
+  }
+    
+  filter_obs <- x_data %>%
+    dplyr::select(replicate, time, Nsurv) %>%
+    dplyr::mutate(replicate = as.character(replicate)) %>%
+    dplyr::filter(!is.na(Nsurv)) %>%
     group_by(replicate) %>%
-    dplyr::mutate(Nsurv_post = ifelse( tNsurv == max(tNsurv), 0, dplyr::lead(Nsurv)),
+    dplyr::mutate(Nsurv_post = ifelse( time == max(time), 0, dplyr::lead(Nsurv)),
                   Nsurv_diff = Nsurv - Nsurv_post,
                   nrow_label = row_number(),
-                  max_tNsurv = max(tNsurv))
-  
+                  max_time = max(time))
+
   # Estimation of probabiblity of survival
+  if(is.null(newdata)){
+    
+    mat_psurv <- t(rstan::extract(x$stanfit, pars = c("Psurv_hat"))$Psurv_hat)
+      
+  } else {
+    
+    mat_psurvPredict <- psurv_predict(x, newdata, draws, interpolate_length, interpolate_method, mc.cores)
+    
+    # inner_join
+    toJoin_posterior_predict <- dplyr::select(mat_psurvPredict, -conc)
+    
+    toJoin_newdata <- filter_obs %>%
+      dplyr::select(-c(Nsurv_post, Nsurv_diff, nrow_label, max_time))
+    
+    join_predict <- dplyr::inner_join(x = toJoin_posterior_predict, y = toJoin_newdata, by = c('replicate','time'))
+    
+    mat_psurv <- dplyr::select(join_predict, -c(time, Nsurv, replicate)) %>%
+      as.matrix()
+    
+  }
   
-  mat_psurv <- t(rstan::extract(object$stanfit, pars = c("Psurv_hat"))$Psurv_hat)
-  
-  mat_psurv_post <- matrix(nrow = nrow(mat_psurv), ncol = ncol(mat_psurv))
+  mat_psurv_post <- matrix(NA, nrow = nrow(mat_psurv), ncol = ncol(mat_psurv))
   
   for(i in 1:nrow(mat_psurv)){
-    if(df_obs$tNsurv[i] == df_obs$max_tNsurv[i]){
-      mat_psurv_post[i, 1:ncol(mat_psurv)] = rep(0, ncol(mat_psurv))
+    if(filter_obs$time[i] == filter_obs$max_time[i]){
+      mat_psurv_post[i, 1:ncol(mat_psurv)] <- rep(0, ncol(mat_psurv))
     } else{
-      mat_psurv_post[i, 1:ncol(mat_psurv)] = mat_psurv[(i+1), 1:ncol(mat_psurv)]
+      mat_psurv_post[i, 1:ncol(mat_psurv)] <- mat_psurv[(i+1), 1:ncol(mat_psurv)]
     }
   }
+  
   mat_psurv_diff <- mat_psurv - mat_psurv_post
   
   # Computing of the likelihood
-  mat_log_lik <- df_obs$Nsurv_diff * log(mat_psurv_diff)
+  mat_log_lik <- filter_obs$Nsurv_diff * log(mat_psurv_diff)
   
   ## For the package loo, the matrix as to be An S by N matrix, where
   ## S is the size of the posterior sample (the number of simulations) and
