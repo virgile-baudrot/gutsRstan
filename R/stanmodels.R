8b238434e243e4c29c67aa3b533ff844d20419cc
vbaudrot <virgile.baudrot@posteo.net>
2018-06-15 17:09:31 +0200

    remove references to methods package which is internal to R

--- R/stanmodels.R
+++ R/stanmodels.R
@@ -8,7 +8,7 @@ stan_files <- dir(MODELS_HOME, pattern = "stan$", full.names = TRUE)
 
 stanmodels <- sapply(stan_files, function(f) {
   model_cppname <- sub("\\.stan$", "", basename(f))
-  isystem <- system.file("chunks", package = methods::getPackageName(environment(), FALSE))
+  isystem <- system.file("chunks", package = getPackageName(environment(), FALSE))
   if (!file.exists(file.path(isystem, "common_functions.stan")))
     isystem <- file.path("inst", "chunks")
   if (!file.exists(file.path(isystem, "common_functions.stan")))
@@ -16,7 +16,7 @@ stanmodels <- sapply(stan_files, function(f) {
   stanfit <- rstan::stanc_builder(f, isystem, allow_undefined = TRUE)
   stanfit$model_cpp <- list(model_cppname = stanfit$model_name, 
                             model_cppcode = stanfit$cppcode)
-  return(do.call(methods::new, args = c(stanfit[-(1:3)], Class = "stanmodel", 
+  return(do.call(new, args = c(stanfit[-(1:3)], Class = "stanmodel", 
                  mk_cppmodule = function(x) get(paste0("model_", model_cppname)))))
   }
 )
